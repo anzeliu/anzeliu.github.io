395718a7afce8c4a5d77a7f19cd069f7c4f9bd81
anzeliu <anzeliu0604@berkeley.edu>
2022-07-25 13:04:57 -0700

    style

--- urls.py
+++ urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 import views
 
 urlpatterns = [
-    path('index', views.index, name='index')
+    path('index', views.index, name='index'),
+    path('projects', views.projects, name='projects')
 ]
--- views.py
+++ views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def projects(request):
+    return render(request, 'projects.html')
